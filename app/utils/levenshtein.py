e651db7a62d135e807f2e4e519d1ffd7232f2c28
Albin Varghese <albin8285@gmail.com>
2025-01-28 15:43:05 +0530

    feat: add levenshetein algorithm util

---
+++ app/utils/levenshtein.py
@@ -0,0 +1,29 @@
+# app/utils/levenshtein.py
+
+def levenshtein(s1: str, s2: str) -> int:
+    """
+    Compute the Levenshtein distance between two strings.
+
+    Args:
+        s1 (str): The first string.
+        s2 (str): The second string.
+
+    Returns:
+        int: The Levenshtein distance between the two strings.
+    """
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+    if not s2:
+        return len(s1)
+
+    prev_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        curr_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = prev_row[j + 1] + 1
+            deletions = curr_row[j] + 1
+            substitutions = prev_row[j] + (c1 != c2)
+            curr_row.append(min(insertions, deletions, substitutions))
+        prev_row = curr_row
+
+    return prev_row[-1]
